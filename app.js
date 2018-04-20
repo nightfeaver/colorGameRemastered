c10fc834cf9c9dd615ebfd97a185e95481f9ca35
Jon <jphamps@gmail.com>
2018-04-20 17:18:14 +0100

    random colors and reset

--- app.js
+++ app.js
@@ -1,22 +1,66 @@
 console.log('connected');
 // change to test github deployment
 
-var colors = ['rgb(255, 0, 0)',
-				'rgb(255, 255, 0)',
-				'rgb(0,255, 0)',
-				'rgb(0,255, 255)',
-				'rgb(0, 0, 255)',
-				'rgb(255, 0, 255)'
-				];
-
+var colors;
+var targetColor;
+var pickedColor;
+var selectedSquare;
 var squares = document.querySelectorAll('.square');
 console.log(squares);
 
-squares.forEach(function(element,index) {
-element.style.backgroundColor=colors[index];
-element.addEventListener('click',doSomething) 
-});
+setUp();
+
+function setUp() {
+	squares.forEach(function(element,index) {
+		element.style.backgroundColor=setColors();
+		element.addEventListener('click',doSomething) 
+	});
+	targetColor=squares[Math.floor(Math.random()*squares.length+1)].style.backgroundColor;
+	document.getElementById('masthead').style.backgroundColor='black';
+	document.getElementById('displayColor').innerHTML=targetColor;
+	document.getElementById('reset').addEventListener('click',setUp);
+	document.getElementById('reset').addEventListener('mouseover',function() {
+		document.getElementById('reset').style.color='yellow'
+	});
+	document.getElementById('reset').addEventListener('mouseout',function() {
+		document.getElementById('reset').style.color='white'
+	});
+}
+
+function setColors() {
+	var r=Math.floor(Math.random()*256);
+	var g=Math.floor(Math.random()*256);
+	var b=Math.floor(Math.random()*256);
+	return 'rgb('+r+' '+g+' '+b+')';
+
+}
 
 function doSomething() {
-	alert('something selected');
+	
+	pickedColor=this.style.backgroundColor;
+	selectedSquare=this;
+	console.log('target color is '+targetColor);
+	console.log(pickedColor);
+	if(pickedColor===targetColor) {
+		document.getElementById('colorMessage').innerHTML='Correct';
+		correct();
+	} else {
+		document.getElementById('colorMessage').innerHTML='Wrong';
+		incorrect(this);
+	}
 }
+
+function incorrect(square) {
+	square.style.backgroundColor='black';
+}
+
+function correct() {
+	squares.forEach(function(element) {
+element.style.backgroundColor=targetColor; 
+});
+	document.getElementById('masthead').style.backgroundColor=targetColor;
+}
+
+
+
+// alert(this.style.backgroundColor);
