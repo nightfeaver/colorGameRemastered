2d17fab02ff8cb34052f651c1d9526b2b1e32ccb
Jon <Jon@Jons-MacBook-Pro.local>
2018-04-21 21:39:02 +0100

    easy and hard mode

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-console.log('connected');
-// change to test github deployment
-
-var colors;
+var numColors=6;
 var targetColor;
 var pickedColor;
 var selectedSquare;
@@ -10,34 +7,62 @@ var squares = document.querySelectorAll('.square');
 setUp();
 
 function setUp() {
-	squares.forEach(function(element,index) {
-		element.style.backgroundColor=setColors();
-		//console.log(element.style.backgroundColor);
-		element.addEventListener('click',doSomething) 
-	});
-	targetColor=squares[Math.floor(Math.random()*squares.length+1)].style.backgroundColor;
-	//console.log('target color is' +targetColor);
-	document.getElementById('masthead').style.backgroundColor='black';
-	document.getElementById('displayColor').innerHTML=targetColor;
-	document.getElementById('reset').addEventListener('click',setUp);
-	document.getElementById('reset').addEventListener('mouseover',function() {
-		document.getElementById('reset').style.color='yellow'
-	});
-	document.getElementById('reset').addEventListener('mouseout',function() {
-		document.getElementById('reset').style.color='white'
-	});
+	configureSquares();
+	configureMasthead();
 }
+
+function configureSquares() {
+	squares.forEach(function(square,index) {
+		if(index<numColors) {
+			square.style.backgroundColor=setColors();
+			square.style.display='block';
+		};
+		if(index>=numColors) {square.style.display='none';};
+		square.addEventListener('click',checkAnswer) 
+	});
+	targetColor=squares[Math.floor(Math.random()*numColors)].style.backgroundColor;
+}	
 
 function setColors() {
 	var r=Math.floor(Math.random()*256);
 	var g=Math.floor(Math.random()*256);
 	var b=Math.floor(Math.random()*256);
 	return 'rgb('+r+', '+g+', '+b+')';
-
 }
 
-function doSomething() {
-	
+function configureMasthead() {
+	document.getElementById('masthead').style.backgroundColor='black';
+	document.getElementById('displayColor').innerHTML=targetColor;
+	document.getElementById('newColor').addEventListener('click',setUp);
+	document.getElementById('newColor').addEventListener('mouseover',function() {
+		document.getElementById('newColor').style.color='purple'
+	});
+	document.getElementById('newColor').addEventListener('mouseout',function() {
+		document.getElementById('newColor').style.color='blue'
+	});
+		document.querySelector('.hard').addEventListener('click',hard);
+		document.querySelector('.easy').addEventListener('click',easy);
+}
+
+function easy() {
+	console.log('easy');
+	if(numColors==3) {console.log('already set');return};
+	document.querySelector('.easy').classList.add('selected');
+	document.querySelector('.hard').classList.remove('selected');
+	numColors=3;
+	setUp();
+}
+
+function hard() {
+	console.log('hard');
+	if(numColors==6) {console.log('already set');return};
+	document.querySelector('.hard').classList.add('selected');
+	document.querySelector('.easy').classList.remove('selected');
+	numColors=6;
+	setUp();
+}
+
+function checkAnswer() {
 	pickedColor=this.style.backgroundColor;
 	selectedSquare=this;
 	console.log(pickedColor);
@@ -60,7 +85,3 @@ element.style.backgroundColor=targetColor;
 });
 	document.getElementById('masthead').style.backgroundColor=targetColor;
 }
-
-
-
-// alert(this.style.backgroundColor);
